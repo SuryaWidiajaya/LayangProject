95de1f9148b0a9ea633fe7bce50147c86e76bb97
I Made Surya Widiajaya Putra <131197072+SuryaWidiajaya@users.noreply.github.com>
2023-12-23 02:14:45 +0800

    Delete FRONTEND directory

--- FRONTEND/src/components/Buttons/ButtonArticle.jsx
+++
@@ -1,7 +0,0 @@
-export default function ButtonArticle(){
-    return(
-        <button className="bg-[#27005D] rounded-[10px] py-[14px] px-[42px] text-white font-Poppins text-[16px] font-semibold hover:bg-[#0F0024]">
-            Selengkapnya
-        </button>
-    )
-}
