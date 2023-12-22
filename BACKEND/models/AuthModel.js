e83aee8b0fa35baf57d643446ae22fa32d79913e
I Made Surya Widiajaya Putra <131197072+SuryaWidiajaya@users.noreply.github.com>
2023-12-23 02:14:18 +0800

    Delete BACKEND directory

--- BACKEND/models/AuthModel.js
+++
@@ -1,8 +0,0 @@
-// AuthModel.js
-import { db } from '../config/Database.js';
-
-export const getUserByEmail = async (email) => {
-  const query = 'SELECT * FROM user WHERE email = ?';
-  const [result] = await db.query(query, [email]);
-  return result[0];
-};
--- BACKEND/routes/agendaRoutes.js
+++
@@ -1,9 +0,0 @@
-
-import express from 'express';
-import * as AgendaController from '../controllers/AgendaController.js';
-
-const router = express.Router();
-
-router.post('/kelurahan_id', AgendaController.getAgendaByKelurahan);
-
-export default router;
