4b3de5e603bdd601bdb087c4322941772b3f26bc
codegod61 <traceb200@gmail.com>
2023-12-23 01:59:13 +0700

    Final Projet Website LAYANG Front-End & Back-End

---
+++ Back-End/index.js
@@ -0,0 +1,47 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { createTable } from './config/Database.js'; // Adjust the path accordingly
+import ArticleRoute from './routes/ArticleRoute.js';
+import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import agendaRoutes from './routes/agendaRoutes.js';
+import adminKelurahanRoutes from './routes/adminKelurahanRoutes.js';
+import pengajuanRoutes from './routes/pengajuanRoutes.js';
+import daerahRoutes from './routes/daerahRoutes.js';
+import laporanRoutes from './routes/laporanRoutes.js'
+import fileUpload from 'express-fileupload';
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api',ArticleRoute);
+app.use(userRoutes);
+app.use(pengajuanRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/agenda', agendaRoutes);
+app.use(adminKelurahanRoutes);
+app.use(laporanRoutes);
+app.use('/api/daerah',daerahRoutes)
+
+const startServer = async () => {
+  try {
+    await createTable();
+
+    console.log('Database terhubung dan tabel berhasil dibuat.');
+
+    app.listen(PORT, () => {
+      console.log(`Server berhasil di running di port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
+};
+
+startServer();
---
+++ Front-End/src/components/Header_Footer/Header.jsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+export default function Header() {
+  return (
+    <>
+      <div className="flex justify-center ">
+        <div className="navbar bg-opacity-0 bg-transparent z-50 w-[1128px] mt-[31px]">
+          <div className="header flex items-center justify-between text-black">
+            <div className="logo flex items-center font-bold text-2xl cursor-pointer">
+              <img className="mr-[19px]" src="img/logoBlack.png" alt="" />
+                <h1>LAYANG</h1>
+            </div>
+
+            <ul className="flex ">
+              <li className="space-x-12 text-black ml-[56px]">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="space-x-12 text-black ml-[56px]">
+                <Link href="./Product">Produk Kami</Link>
+              </li>
+              <li className="space-x-12 text-black ml-[56px]">
+                <Link href="./Profile ">Tentang Kami</Link>
+              </li>
+              <li className="space-x-12 text-black ml-[56px]">
+                <Link href="./ArticleNews">Artikel</Link>
+              </li>
+
+              <li className="space-x-12 text-black ml-[56px]">
+                <Link href="./Contact">Kontak Kami</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
